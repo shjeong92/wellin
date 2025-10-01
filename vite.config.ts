8f8db174865f458db934428ebee45d0dd66c2ff7
Sanghyuk Jeong <jade@indentcorp.com>
2025-10-02 00:11:47 +0900

    Configure Vite for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: mode === 'production' ? '/wellin/' : '/',
+      base: process.env.VERCEL ? '/' : (mode === 'production' ? '/wellin/' : '/'),
       plugins: [react()],
       resolve: {
         alias: {
